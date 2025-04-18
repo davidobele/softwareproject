c4f30c3f9d4a800c7eed79e266cf38d2107525ce
dobele <davidobele20@gmail.com>
2025-04-18 10:29:14 -0400

    Your commit message

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <>
+      <header>
+        <nav>
+          <button>Log in</button>
+          <button>Sign Up</button>
+          <button>About</button>
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
